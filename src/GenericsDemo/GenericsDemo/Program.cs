379950ae4292e5fce6799860e8815185116d80dc
melaniesdavis <melaniesdavis@hotmail.com>
2017-02-12 08:50:50 -0500

    Added EnumberableCompositor

---
+++ src/GenericsDemo/GenericsDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        public static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+
+        static void Main(string[] args)
+        {
+            var list1 = new List<int> { 1, 2, 3, 4, 5 };
+            var list2 = new List<int> { 2, 4, 6, 8, 10 };
+            var set1 = new HashSet<int> { 3, 6, 9, 12, 15 };
+            var array1 = new[] { 4, 8, 12, 16, 20 };
+            /*        
+                      var ec = new EnumerableCompositor<int> {list1, list2, set1, array1};
+
+                    int numOdd = 0;
+
+                    foreach (var value in ec)
+                    {
+                      if(IsOdd(value))
+                      {
+                        numOdd++; 
+                      }
+                    }
+
+                      int numOdd = ec.Count(x => IsOdd(x));*/
+        }
+    }
+}
